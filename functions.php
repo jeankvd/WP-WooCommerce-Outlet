8a06d86853bf2783cb0c2c3cfa64279425021b12
jeankvd <djjean85@gmail.com>
2018-01-13 21:28:58 -0500

    shop template structure and styles

--- functions.php
+++ functions.php
@@ -169,3 +169,6 @@ add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
+
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+add_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 10 );
